87dddbc765bdfe10f8569b5bc164b61dbd3611cf
Daniel Otis <danielotis@Daniels-MacBook-Pro.local>
2023-05-22 23:38:49 -0600

    Updates

---
+++ precache-manifest.df5c9a8488f262f3a12210a7628c8268.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3513ec2cc292e9c6b12d12bb65efb6b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3fbf4dfb6a8676cc6545",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6ea89a95a29444c62726",
+    "url": "/portfolio/static/css/main.a2105dd5.chunk.css"
+  },
+  {
+    "revision": "3fbf4dfb6a8676cc6545",
+    "url": "/portfolio/static/js/2.54d8b74d.chunk.js"
+  },
+  {
+    "revision": "e4c137de68f1dc6b72d1a2fbfaf8fb8f",
+    "url": "/portfolio/static/js/2.54d8b74d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ea89a95a29444c62726",
+    "url": "/portfolio/static/js/main.c546dd5f.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "5c0a4ff97a3576d1b2f60a7a11a12539",
+    "url": "/portfolio/static/media/ai-image-generator.5c0a4ff9.png"
+  },
+  {
+    "revision": "541559a5ae6e5dc8746bebe93835ef9f",
+    "url": "/portfolio/static/media/blog.541559a5.png"
+  },
+  {
+    "revision": "77b318977a51f25e06bf872dc336dfb6",
+    "url": "/portfolio/static/media/pirate-panic.77b31897.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio/static/media/skillImage.59389695.svg"
+  }
+]);
